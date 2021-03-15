22c580c42f7c23db5e5f2a24f74ee35aeb8bd409
Yahua Yan <yanyahua@joinquant.com>
2021-03-15 20:58:31 +0800

    support asm global variable

--- tags/asm.py
+++ tags/asm.py
@@ -23,16 +23,29 @@ def find_tags(abspath):
             for li in lines:
                 li = li.strip()
                 line_number += 1
+                semi_index = li.find(';')
+                if semi_index >= 0:
+                    li = li[:semi_index]
+                    li = li.strip()
                 if not li:
                     continue
-                if li[0]  == ';':
-                    continue
+
                 if li.endswith(':'):
                     match_obj = re.search(identifier, li[:-1])
                     if match_obj is not None and match_obj.group() == li[:-1]:
                         tags.append([
                             li[:-1], line_number, LangType.lang_c + LangType.def_func
                         ])
+                eq_index = li.find('=')
+                if eq_index >= 0:
+                    word = li[:eq_index].strip()
+                    match_obj = re.search(identifier, word)
+                    if match_obj is not None and match_obj.group() == word:
+                        tags.append([
+                            word, line_number, LangType.lang_c + LangType.def_variable
+                        ])
+
+
 
         except:
             import traceback
