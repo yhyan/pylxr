8db3e87ca46f279f3540d0a69760a42c15eaf967
Yahua Yan <yanyahua@joinquant.com>
2021-04-10 23:17:22 +0800

    fix smart read

--- utils.py
+++ utils.py
@@ -8,7 +8,9 @@ def smart_read(abspath):
         txt = fp.read()
     result = chardet.detect(txt)
     encoding = result['encoding']
-
+    if not encoding:
+        return txt
+    
     # GB2312，GBK，GB18030，是兼容的，包含的字符个数：GB2312 < GBK < GB18030
     if encoding.lower() == 'gb2312':
         encoding = 'gb18030'
