dbf3ac7daa3c4ad27b7ef8dd152a628eeef8414c
yhyan <yanjiahua90@163.com>
2021-05-28 18:09:34 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@
 import chardet
 
 def smart_read(abspath, retrun_lines=False):
+    txt = ''
     with open(abspath, 'rb') as fp:
         txt = fp.read()
     result = chardet.detect(txt)
@@ -12,8 +13,11 @@ def smart_read(abspath, retrun_lines=False):
         # GB2312，GBK，GB18030，是兼容的，包含的字符个数：GB2312 < GBK < GB18030
         if encoding.lower() == 'gb2312':
             encoding = 'gb18030'
-
-        txt = txt.decode(encoding)
+        try:
+            txt = txt.decode(encoding)
+        except:
+            with open(abspath) as fp:
+                txt = fp.read()
     if retrun_lines:
         return txt.splitlines(True)
     return txt
